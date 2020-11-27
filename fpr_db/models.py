a38c52f9444ea545aa8140e1e40549b64b1ce671
Luke Troyer <luketroyer@MacBook-Pro-5.local>
2020-11-27 08:54:31 -0800

    Added a basic employee Model

--- fpr_db/models.py
+++ fpr_db/models.py
@@ -8,6 +8,11 @@ class Parent(models.Model):
     email = models.CharField(max_length=50)
     phone = models.CharField(max_length=14)
 
+class Employee(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=14)
 
 class Camp(models.Model):
     name = models.CharField(max_length=40)
