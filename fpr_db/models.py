db5821f4bea2a85528bb4659b558a0f20d6f31e1
Luke Troyer <luketroyer@MacBook-Pro-5.local>
2020-12-03 12:56:13 -0800

    test change 12:56

--- fpr_db/models.py
+++ fpr_db/models.py
@@ -8,13 +8,12 @@ class Parent(models.Model):
     email = models.CharField(max_length=50)
     phone = models.CharField(max_length=14)
 
-#test
 class Employee(models.Model):
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     phone = models.CharField(max_length=14)
-
+#this ia a test to see if i can go to my branch
 class Camp(models.Model):
     name = models.CharField(max_length=40)
     #student = models.ForeignKey(Student, on_delete=models...)
